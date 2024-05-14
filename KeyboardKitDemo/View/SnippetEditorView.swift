272c7ac580fefe7c24cbc9a22d2b228dbc9d91b2
Bing <6bingj@gmail.com>
2024-05-13 22:00:58 -0700

    Add main app functionality on snippet editing

---
+++ KeyboardKitDemo/View/SnippetEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  SnippetEditorView.swift
+//  KeyboardKitDemo
+//
+//  Created by Bingjian Liu on 5/13/24.
+//
+
+import SwiftUI
+
+struct SnippetEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SnippetEditorView()
+}
